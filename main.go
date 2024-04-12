6adf39881f70a1c34f4c04910a8c14f69141c1ba
vijendra sukariya <vijju8769310002@gmail.com>
2024-04-12 17:52:27 +0530

    remove all the functions

--- main.go
+++ main.go
@@ -2,28 +2,6 @@ package main
 
 import "fmt"
 
-func Sub(a, b int) int {
-	if a == b {
-		return 0
-	}
-	return a - b
-}
-
-func Sum(a, b int) int {
-	return a + b
-}
-
-func Mul(a, b int) int {
-	return a * b
-}
-
-func Div(a, b int) int {
-	if b == 0 {
-		return -1
-	}
-	return a / b
-}
-
 func main() {
 	fmt.Println("mains")
 }
